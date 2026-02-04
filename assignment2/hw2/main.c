d725cb43d3f2c1cccd894d9dd83db5893b76c8e3
Alex Ly <Alex_Ly@student.uml.edu>
2026-02-04 12:25:53 -0500

    commit for chicken break

--- assignment2/hw2/main.c
+++ assignment2/hw2/main.c
@@ -1,28 +1,34 @@
 #include "functions.h"
 
-int main(int argc, char* argv[])
+int main()
 {
+    float float1, float2, hardware, emulated,
+        float_32, float_1, float_2, hardware_32, emulated_32;
+    int valid = 1;
 
     // Boilerplate text
     printf("Please enter two positive floating point values each with:\n");
     printf("    - no more than 6 significant digits\n");
     printf("    - a value between   +   10**37 and 1-**-37\n");
+
     printf("Enter Float 1:  ");
-    // Scanf
+    valid = scanf("%f", &float1);
+
     printf("Enter Float 2:  ");
-    // Scanf
+    valid = scanf("%f", &float2);
+
+    printf("\n");
 
     printf("Original pattern of Float 1:    ");
     // Function output
+    // XX
 
-    // Pattern of Float 2
     printf("Original pattern of Float 2:    ");
     // Function output
 
     // Do Bit pattern of result function
-
-    // Bit pattern of combo
     printf("Bit pattern of result:  ");
+    // Bit pattern of result
 
     // Emulated float result from printf float value 
     printf("EMULATED FLOATING RESULT FROM PRINTF ==>>> \n");
