63e8da61395e95be236af0d95286272d4b2970f6
Alex Ly <Alex_Ly@student.uml.edu>
2026-02-18 20:24:01 -0500

    Completed Assignment 3

--- assignment3/hw3/fib.asm
+++ assignment3/hw3/fib.asm
@@ -1,50 +1,54 @@
 LOOP:   
-	LODD PasCnt:  		
-	JZER DONE:		
-	SUBD c1:
-	STOD PasCnt:		
+	LODD PasCnt:		; num of fibs to do in PasCnt
+	JZER DONE:			; no more passes, go to done
+	SUBD c1:			; - - passes remaining
+	STOD PasCnt:		; load a pointer to fib arg
 P1:	
-	LODD daddr:		
-	PSHI			
-	ADDD c1:
-	STOD daddr:		
-	CALL FIB:		
-	INSP 1			
+	LODD daddr:			; inc, store pointer for next d[n] 
+	PSHI				; call fib (arg on stack)
+	ADDD c1:			; clear stack on fib return
+	STOD daddr:			; put return AC (fib(n)) on stack
+	CALL FIB:			; load a pointer to result f[n]
+	INSP 1				; pop result off stack into f[n]
 P2:	
-	PUSH			
-	LODD faddr:		
-	POPI			
-	ADDD c1:
-	STOD faddr:		
-	JUMP LOOP:		
+	PUSH				; inc, store pointer for next f[n]
+	LODD faddr:			; go to top for next pass
+	POPI				; pop result off stack into f[n]
+	ADDD c1:			
+	STOD faddr:			; inc, store pointer for next f[n]
+	JUMP LOOP:			; go to top for next pass
+
+; -------------------- Recursive implementation -----------------------
 
 FIB:	
-	LODL 1
-	JZER FIBZER:
-	SUBD c1:
-	JZER FIBONE:
-	PUSH
-	CALL FIB:
-	PUSH
-	LODL 1
-	SUBD c1:
-	PUSH
-	CALL FIB:
+	LODL 1				; Load argument N from stack
+	JZER FIBZER:		; if N == 0 return 0 base case
+	SUBD c1:			; AC = N - 1
+	JZER FIBONE:		; if N == 1, return 1 second base case
+	PUSH				; Push (N-1) as argument for recursive call
+
+	CALL FIB:			; Recursive call on FIB(N-1)
+	PUSH				; Store FIB(N-1) onto stack
+	LODL 1				; Load the value from the stack
+	SUBD c1:			; Subtract c1 (1) from AC
+	PUSH				; Push FIB(N-2) as argument for recursive call
+	CALL FIB:			; Call FIB(N-2)
 
 CLEAN:
-	INSP 1
-	ADDL 0
-	INSP 2
-	RETN
-
-
+	INSP 1				; Clear FIB(N-2) from the stack (top most value)
+	ADDL 0				; Store the result into AC
+	INSP 2				; Clear arguments from the stack
+	RETN				; Return
 
 FIBZER:	
 	LODD c0:
-	RETN			
+	RETN
+
 FIBONE:	
 	LODD c1:
 	RETN			
+
+; -------------------- End of Recursive implementation -----------------------
 
 DONE:	HALT	 
 .LOC 	100		
@@ -61,7 +65,5 @@ f0:  	0
 daddr:  d0:			
 faddr:  f0:			
 c0: 	0			
-c1: 	1
-c2: 	2			
+c1: 	1	
 PasCnt: 5			
-tmp:	0			
